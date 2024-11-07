e62eada27822ca858004c88249a2b0255579a1d0
Sandipan Adhikary <sandipanadhikary01@gmail.com>
2024-11-07 12:59:15 +0530

    latest12 commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { HashRouter,Routes,Route } from 'react-router-dom'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
@@ -10,39 +8,7 @@ import Contacts from './components/Contacts/Contacts.jsx'
 import Github from './components/Github/Github.jsx'
 import User from './components/User/User.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: "",
-        element: <Home />
-      },
-      {
-        path: "about",
-        element: <About />
-      },
-      {
-        path: "contacts",
-        element: <Contacts />
-      },
-      {
-        path: "github",
-        element: <Github />
-      },
-      {
-        path: "user/:id",
-        element: <User />
-      }
-    ]
-  }
-],
-{
-  basename: "/Pied-Piper",
-})
-
-createRoot(document.getElementById('root')).render(
+document.getElementById('root').render(
   <StrictMode>
     <HashRouter>
       <Routes>
